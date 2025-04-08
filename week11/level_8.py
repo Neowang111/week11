fd8a01f647a503acce3f6e3a08438db0d94154ea
王一涵 <wangyihan@Neos-MacBook-Pro-2.local>
2025-04-08 19:50:43 -0400

    Level 8 complete

---
+++ week11/level_8.py
@@ -0,0 +1,11 @@
+
+# Level 8: Exploring the input() function
+
+print("Test 1: No prompt in input()")
+name1 = input()
+print("You entered:", name1)
+
+question = "What's your favorite color? "
+print("Test 2: Using a variable as the prompt")
+color = input(question)
+print("You entered:", color)
