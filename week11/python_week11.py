94cfe22c66d4b620a169ef5c18c147f100cfab74
王一涵 <wangyihan@Neos-MacBook-Pro-2.local>
2025-04-08 19:21:34 -0400

    Level 5 Complete

--- week11/python_week11.py
+++ week11/python_week11.py
@@ -13,3 +13,19 @@ z = None
 items = ["apple", 5, False]  
 
 print("Level 4 running!")
+
+"""
+Variables can be ANYTHING, because it's just a name, as long as they follow these rules.
+
+ 1. Variable names must start with a letter (a-z, A-Z) or an underscore (_)
+ 2. The rest of the variable name can contain letters, numbers, or underscores
+ 3. Variable names are case-sensitive (myVar ≠ myvar)
+ 4. Variable names cannot be Python keywords (like `class`, `if`, `while`, etc.)
+5. Variable names cannot start with a number
+
+ Examples of **invalid** variable names:
+3dogs = "bad"          # Starts with a number
+my-variable = 5        # Hyphen is not allowed
+def = "nope"           # 'def' is a reserved keyword
+$$$Variable = "bad"    # Special symbols are not allowed
+"""
