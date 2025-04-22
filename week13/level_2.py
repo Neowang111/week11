aaaf5d8a78d481ca73787c7e1da85c61d29d040a
王一涵 <wangyihan@Neos-MacBook-Pro-2.local>
2025-04-22 19:02:53 -0400

    Level 2 Complete

---
+++ week13/level_2.py
@@ -0,0 +1,10 @@
+playlist_for_coding = [
+    "Time - Pink Floyd",
+    "Bohemian Rhapsody - Queen",
+    "Clair de Lune - Debussy",
+    "Everything In Its Right Place - Radiohead",
+    "Weightless - Marconi Union"
+]
+print(playlist_for_coding[2])
+playlist_for_coding.append("Never Gonna Give You Up by Rick Astley")
+print(playlist_for_coding)
